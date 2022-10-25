1e5a11a37f0a2cf5f00de69e5f0c3936b3184de1
Виктория Князькова <72147784+vknyazkova@users.noreply.github.com>
2022-10-25 16:11:51 +0300

    add removeBlock

--- search/web_app/static/js/scripts.js
+++ search/web_app/static/js/scripts.js
@@ -1,19 +1,34 @@
 let i = 1;
 
 function duplicate() {
-    let original = document.getElementById('duplicater' + i);
+    let original = document.getElementById('duplicater_' + i);
     let clone = original.cloneNode(true); // "deep" clone
-    clone.id = "duplicater" + ++i;
-
-    let inputs = clone.getElementsByTagName("input")
-    inputs[0].setAttribute("name", i + "_from")
-    inputs[1].setAttribute("name", i + "_to")
-    inputs[2].setAttribute("name", i + "_word")
-    inputs[3].setAttribute("name", i + "_features")
-
-    let select = clone.getElementsByTagName('select')
-    select[0].setAttribute("name", i + '_pos')
-
+    updateBlockID(clone, ++i)
     original.parentNode.appendChild(clone);
+}
 
+function updateBlockID(block, id){
+    block.id = "duplicater_" + id
+    let inputs = block.getElementsByTagName("input");
+    inputs[0].setAttribute("name", id + "_from");
+    inputs[1].setAttribute("name", id + "_to");
+    inputs[2].setAttribute("name", id + "_word");
+    inputs[3].setAttribute("name", id + "_features");
+
+    let select = block.getElementsByTagName('select');
+    select[0].setAttribute("name", id + '_upos');
+
+    let button = block.getElementsByTagName('a');
+    button[button.length-1].setAttribute("id", 'close_' + id);
+}
+
+
+function removeBlock(clicked_id) {
+    let id = Number(clicked_id.split('_')[1]);
+    let all_blocks = document.querySelectorAll('[id^="duplicater"]');
+    document.getElementById('duplicater_' + id).remove();
+    --i
+    for (let k = id; k < all_blocks.length; k++) {
+        updateBlockID(all_blocks[k], Number(all_blocks[k].id.split('_')[1]) - 1)
+    }
 }
